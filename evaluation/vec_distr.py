dd1b1061e67717306e036ddffa879ffd99925c73
guenthermi <guenthermi50@yahoo.de>
2018-01-17 21:17:26 +0100

    add calculation of farthest vectors to vec_distr

--- evaluation/vec_distr.py
+++ evaluation/vec_distr.py
@@ -18,6 +18,26 @@ import index_utils as utils
 SAMPLE_SIZE = 100
 STEPS_K = 6
 
+def updateTopK(topk, id, sim, negative=True):
+    if (sim < topk[STEPS_K-1][1]) == negative:
+        i = 0
+        tmp_id = -1
+        tmp_sim = 0
+        while (i < STEPS_K):
+            if (sim < topk[i][1]) == negative:
+                tmp_id = topk[i][0]
+                tmp_sim = topk[i][1]
+                topk[i] = (id, sim)
+                break
+            i += 1
+        for j in range(1, STEPS_K-i):
+            tmp_id_new = topk[i+j][0]
+            tmp_sim_new = topk[i+j][1]
+            topk[i+j] = (tmp_id, tmp_sim)
+            tmp_id = tmp_id_new
+            tmp_sim = tmp_sim_new
+    return
+
 def calculate_similarity_values(words, vecs, size):
     sampleIds = [random.randint(0, size) for x in range(SAMPLE_SIZE)]
     # init faiss
@@ -26,6 +46,7 @@ def calculate_similarity_values(words, vecs, size):
 
     # calculate TopK
     similarities = []
+    dissimilarities = []
     for i in range(STEPS_K):
         similarities.append([])
     for i, sampleId in enumerate(sampleIds):
@@ -34,9 +55,19 @@ def calculate_similarity_values(words, vecs, size):
             # measure similarity
             cos_sim = np.dot(vecs[sampleId], vecs[I[0][j]])
             similarities[j].append(cos_sim)
-    return similarities
+    # calculate farest neighbors
+    for i in range(STEPS_K):
+        dissimilarities.append([])
+    for i, sampleId in enumerate(sampleIds):
+        topk = [(-1,1) for x in range(STEPS_K)]
+        for j, vec in enumerate(vecs[:size]):
+            cos_sim = np.dot(vecs[sampleId], vecs[j])
+            updateTopK(topk, j, cos_sim)
+        for j in range(STEPS_K):
+            dissimilarities[j].append(topk[j][1])
+    return similarities, dissimilarities
 
-def plot_graph(sim_values):
+def plot_graph(sim_values, dis_sim_values):
     plot_data = [
         go.Scatter(
             x=[n+1 for n in range(len(sim_values))],
@@ -45,19 +76,38 @@ def plot_graph(sim_values):
                 type='data',
                 visible=True,
                 array=[np.mean([abs(np.mean(x) - y) for y in x])*180/np.pi for x in sim_values]
-            )
+            ),
+            name='Nearest Neighbors'
+        ),
+        go.Scatter(
+            x=[n+1 for n in range(len(dis_sim_values))],
+            y=[np.arccos(np.mean(x))*180/np.pi for x in dis_sim_values],
+            error_y=dict(
+                type='data',
+                visible=True,
+                array=[np.mean([abs(np.mean(x) - y) for y in x])*180/np.pi for x in dis_sim_values]
+            ),
+            name='Farest Neighbors'
+
         )
     ]
-    layout = go.Layout(xaxis=dict(nticks=STEPS_K, tickfont=dict(size=20)), yaxis= dict(range=[0,90], title='angle in degree', titlefont=dict(size=20), tickfont=dict(size=20)))
+    print([np.arccos(np.mean(x))*180/np.pi for x in sim_values])
+    print([np.arccos(np.mean(x))*180/np.pi for x in dis_sim_values])
+    layout = go.Layout(xaxis=dict(nticks=STEPS_K, tickfont=dict(size=20)), yaxis= dict(range=[0,180], title='angle in degree', titlefont=dict(size=20), tickfont=dict(size=20)))
     fig = go.Figure(data=plot_data, layout=layout)
     plotly.offline.plot(fig, filename="tmp.html", auto_open=True)
     return None
 
 def main(argc, argv):
-    words, vecs, size = utils.get_vectors('../vectors/google_vecs.txt')
-    sim_values = calculate_similarity_values(words, vecs, size)
+    filename = '../vectors/google_vecs.txt'
+    if argc > 1:
+        filename = argv[1]
+    words, vecs, size = utils.get_vectors(filename)
+    print(size)
+    sim_values, dis_sim_values = calculate_similarity_values(words, vecs, size)
     print(sim_values)
-    plot_graph(sim_values[1:])
+    print(dis_sim_values)
+    plot_graph(sim_values[1:], dis_sim_values[:-1])
 
 
 if __name__ == "__main__":
